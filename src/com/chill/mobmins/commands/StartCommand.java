2d3f43aab41865e61b22abbe4580ee64d5dd1057
Chill Nook <chillbusiness@hotmail.com>
2021-08-15 23:22:43 -0400

    1

---
+++ src/com/chill/mobmins/commands/StartCommand.java
@@ -0,0 +1,17 @@
+package com.chill.mobmins.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class StartCommand implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+
+
+        return true;
+    }
+}
