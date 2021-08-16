03db695c83cf74ae4daf89a547f9e3b96834684d
Chill Nook <chillbusiness@hotmail.com>
2021-08-16 00:06:34 -0400

    Plugin files.

--- src/com/chill/mobmins/MobMins.java
+++ src/com/chill/mobmins/MobMins.java
@@ -2,14 +2,21 @@ package com.chill.mobmins;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MobMins extends JavaPlugin {
+    FileConfiguration config = getConfig();
 
     private static MobMins instance;
 
     @Override
     public void onEnable() {
+
+        config.addDefault("Enabled.", false);
+        config.options().copyDefaults(true);
+
+        saveConfig();
 
         Print(ChatColor.GREEN, "[MobMins] MobMins successfully enabled.");
 
--- src/com/chill/mobmins/commands/StartCommand.java
+++ src/com/chill/mobmins/commands/StartCommand.java
@@ -1,16 +1,49 @@
 package com.chill.mobmins.commands;
 
+import com.chill.mobmins.MobMins;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.IOException;
 
 public class StartCommand implements CommandExecutor {
+    FileConfiguration config = MobMins.getInstance().getConfig();
 
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
+        if(!(sender instanceof Player)) {
 
+            MobMins.Print(ChatColor.YELLOW, "Try running this command again from a player.");
+
+        }
+
+        Player player = (Player) sender;
+
+        if(cmd.getName().equalsIgnoreCase("start")) {
+
+            if(config.getBoolean("Enabled")) {
+
+                config.set("Enabled", true);
+
+                //Config saving
+                try {
+                    MobMins.getInstance().getConfig().save("plugins/MobMins/config.yml");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    MobMins.Print(ChatColor.RED, "Could not save config! Report this to the developer.");
+                }
+
+                //Enable mob event
+
+            }
+
+        }
 
         return true;
     }
